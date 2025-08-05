982933a87b813cf8358ae607de3f0b2418f03be1
Leonardo Lopes <leonardo.lopes@estudante.ufscar.br>
2025-08-05 00:14:34 -0300

    fix opening unlimited database connections

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,34 +3,41 @@ import { join } from "node:path";
 import database from "infra/database.js";
 
 export default async function migrations(request, response) {
-  const databaseClient = await database.getNewClient();
-  const defaultMigrationOptions = {
-    dbClient: databaseClient,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    dryRun: true,
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  };
+  const allowedMethods = ["GET", "POST"];
   const httpMethod = request.method;
-
-  if (httpMethod === "GET") {
-    const pendingMigrations = await migrationRunner(defaultMigrationOptions);
-    await databaseClient.end();
-    return response.status(200).json(pendingMigrations);
+  if (!allowedMethods.includes(httpMethod)) {
+    return response.status(405).end();
   }
-  if (httpMethod === "POST") {
-    const migratedMigrations = await migrationRunner({
-      ...defaultMigrationOptions,
-      dryRun: false,
-    });
-    await databaseClient.end();
-    const migratedMigrationsHaveRun = migratedMigrations.length > 0;
-    if (migratedMigrationsHaveRun) {
-      return response.status(201).json(migratedMigrations);
+  let databaseClient = null;
+  try {
+    databaseClient = await database.getNewClient();
+    const defaultMigrationOptions = {
+      dbClient: databaseClient,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      dryRun: true,
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    };
+
+    if (httpMethod === "GET") {
+      const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+      return response.status(200).json(pendingMigrations);
     }
-    return response.status(200).json(migratedMigrations);
+    if (httpMethod === "POST") {
+      const migratedMigrations = await migrationRunner({
+        ...defaultMigrationOptions,
+        dryRun: false,
+      });
+      const migratedMigrationsHaveRun = migratedMigrations.length > 0;
+      if (migratedMigrationsHaveRun) {
+        return response.status(201).json(migratedMigrations);
+      }
+      return response.status(200).json(migratedMigrations);
+    }
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await databaseClient?.end();
   }
-
-  return response.status(405).end();
 }
