26ef8841b6221d5a36ee271f5b599a7ae6e794d6
Leonardo Lopes <leonardo.lopes@estudante.ufscar.br>
2025-08-13 22:54:25 -0300

    adds `Linting` Github Action

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -4,7 +4,7 @@ import orchestrator from "tests/orchestrator.js";
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
   await database.query("DROP SCHEMA public CASCADE; CREATE SCHEMA public;");
-})
+});
 
 test("GET to /api/v1/migrations should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
