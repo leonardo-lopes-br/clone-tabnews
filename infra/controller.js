8861c0be32c570aa3a1e69cb91b44fa4be719b9b
Leonardo Lopes <leonardo.lopes@estudante.ufscar.br>
2025-10-15 01:13:57 -0300

    feat: implement `/api/v1/user` endpoint with session renewal

--- infra/controller.js
+++ infra/controller.js
@@ -1,3 +1,4 @@
+import * as cookie from "cookie";
 import {
   InternalServerError,
   MethodNotAllowedError,
@@ -5,6 +6,7 @@ import {
   UnauthorizedError,
   ValidationError,
 } from "infra/errors.js";
+import session from "models/session.js";
 
 function onErrorHandler(error, request, response) {
   if (
@@ -26,11 +28,23 @@ function onNoMatchHandler(request, response) {
   response.status(publicErrorObject.statusCode).json(publicErrorObject);
 }
 
+function setSessionCookie(sessionToken, response) {
+  const setCookie = cookie.serialize("session_id", sessionToken, {
+    path: "/",
+    maxAge: session.EXPIRATION_IN_MILLISECONDS / 1000,
+    secure: process.env.NODE_ENV === "production",
+    httpOnly: true,
+  });
+
+  response.setHeader("Set-Cookie", setCookie);
+}
+
 const controller = {
   errorHandlers: {
     onNoMatch: onNoMatchHandler,
     onError: onErrorHandler,
   },
+  setSessionCookie,
 };
 
 export default controller;
--- models/session.js
+++ models/session.js
@@ -1,5 +1,6 @@
 import crypto from "node:crypto";
 import database from "infra/database.js";
+import { UnauthorizedError } from "infra/errors";
 
 const EXPIRATION_IN_MILLISECONDS = 60 * 60 * 24 * 30 * 1000; // 30 days
 
@@ -28,8 +29,66 @@ async function create(userId) {
   }
 }
 
+async function renew(sessionId) {
+  const expiresAt = new Date(Date.now() + EXPIRATION_IN_MILLISECONDS);
+  const renewdSessionObject = await runUpdateQuery(sessionId, expiresAt);
+
+  return renewdSessionObject;
+
+  async function runUpdateQuery(sessionId, expiresAt) {
+    const result = await database.query({
+      text: `
+        UPDATE
+          sessions
+        SET
+          expires_at = $2,
+          updated_at = now()
+        WHERE
+          id = $1
+        RETURNING
+          *
+      ;`,
+      values: [sessionId, expiresAt],
+    });
+    return result.rows[0];
+  }
+}
+
+async function findOneValidByToken(sessionToken) {
+  const sessionFound = await runSelectQuery(sessionToken);
+
+  return sessionFound;
+
+  async function runSelectQuery(sessionToken) {
+    const result = await database.query({
+      text: `
+          SELECT
+            *
+          FROM
+            sessions
+          WHERE
+            token = $1
+            AND expires_at > NOW()
+          LIMIT 1
+        ;`,
+      values: [sessionToken],
+    });
+
+    if (result.rowCount === 0) {
+      throw new UnauthorizedError({
+        message: "Usuário não possui sessão ativa.",
+        action: "Verifique se este usuário está logado e tente novamente.",
+      });
+    }
+
+    return result.rows[0];
+  }
+}
+
 const session = {
   create,
+  renew,
+  findOneValidByToken,
   EXPIRATION_IN_MILLISECONDS,
 };
 
